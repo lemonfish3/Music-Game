694c8e643e27b79bdf6b61b1982879914c39a1cf
lemonfish3 <muqingyang2018@gmail.com>
2025-10-19 16:16:24 -0700

    note scripts

---
+++ Music Game/Assets/Scripts/NoteChart.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[CreateAssetMenu(fileName = "NoteChart", menuName = "Scriptable Objects/NoteChart")]
+public class NoteChart : ScriptableObject
+{
+    [Header("Song Info")]
+    public string songTitle;
+    public AudioClip musicClip;
+    public float BPM = 120f;
+    public float offset = 0f;
+
+    [Header("Notes")]
+    public List<NoteData> notes = new List<NoteData>();
+
+    [Header("Lesson Info")]
+    [TextArea] public string lessonDescription;
+    public bool showNoteNames = true;
+}
---
+++ Music Game/Assets/Scripts/NoteData.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+[System.Serializable]
+public class NoteData
+{
+    [Header("Timing")]
+    public float beat;
+    public float duration;
+
+    [Header("Music Properties")]
+    public NoteType noteType;
+    public string noteName;
+    public Sprite noteSprite;
+
+    [Header("Gameplay Properties")]
+    public int lane;
+    public bool isLongNote;
+    public bool isRestNote;
+
+    public enum NoteType
+    {
+        Tap,
+        Hold,
+        Slide
+    }
+}
+
